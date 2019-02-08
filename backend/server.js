079b3db283e99ae1717096bafc1aa483fb8e9586
Unknown <nick-dallege@uiowa.edu>
2019-02-08 01:40:29 -0600

    replace mongodb with mysql

--- backend/server.js
+++ backend/server.js
@@ -1,90 +1,30 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const Data = require("./schema");
-const credentials = require("./credentials");
-const username = credentials.USERNAME;
+const express = require('express');
+const mysql = require('mysql');
+
+const credentials = require('./credentials');
 const password = credentials.PASSWORD;
+const db = credentials.DB;
 
-const API_PORT = 3001;
 const app = express();
-const router = express.Router();
+const PORT = process.env.PORT || 3000;
 
-// this is our MongoDB database
-const dbRoute = "mongodb://" + username + ":" + password + "@ds221405.mlab.com:21405/vanpool";
 
-// connects our back end code with the database
-mongoose.connect(
-  dbRoute,
-  { useNewUrlParser: true }
-);
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: password,
+  database: db
+})
 
-let db = mongoose.connection;
+require('./routes/html-routes')(app, connection);
 
-db.once("open", () => console.log("connected to the database"));
-
-// checks if connection with the database is successful
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
-// (optional) only made for logging and
-// bodyParser, parses the request body to be a readable json format
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(logger("dev"));
-
-// this is our get method
-// this method fetches all available data in our database
-router.get("/getData", (req, res) => {
-  Data.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
+// if error, log the error message
+connection.connect(function(err) {
+  (err)? console.log(err): console.log(connection);
 });
 
-// this is our update method
-// this method overwrites existing data in our database
-router.post("/updateData", (req, res) => {
-  const { id, update } = req.body;
-  Data.findOneAndUpdate(id, update, err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
-
-// this is our delete method
-// this method removes existing data in our database
-router.delete("/deleteData", (req, res) => {
-  const { id } = req.body;
-  Data.findOneAndDelete(id, err => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
-});
-
-// this is our create methid
-// this method adds new data in our database
-router.post("/putData", (req, res) => {
-  let data = new Data();
-
-  const { id, message } = req.body;
-
-  if ((!id && id !== 0) || !message) {
-    return res.json({
-      success: false,
-      error: "INVALID INPUTS"
-    });
-  }
-  data.message = message;
-  data.id = id;
-  data.save(err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
-
-// append /api for our http requests
-app.use("/api", router);
 
 // launch our backend into a port
-app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+app.listen(PORT, () => {
+  console.log(`LISTENING ON PORT ${PORT}`);
+});
